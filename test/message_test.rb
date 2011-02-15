530c449c2293ce80558d5b8a951d046cec19789f
Blake Mizerany <blake.mizerany@gmail.com>
2011-02-15 11:06:44 -0800

    formatting

--- test/message_test.rb
+++ test/message_test.rb
@@ -290,9 +290,11 @@ class MessageTest < Test::Unit::TestCase
   end
 
   def test_decode_repeated_nested
-    msg = RepeatedNestedMessage.new(:simple =>
-                                    [SimpleMessage.new(:a => 1),
-                                     SimpleMessage.new(:b => "hello")]).encode
+    simple = [
+      SimpleMessage.new(:a => 1),
+      SimpleMessage.new(:b => "hello")
+    ]
+    msg = RepeatedNestedMessage.new(:simple => simple).encode
     got = RepeatedNestedMessage.decode(msg)
     assert_equal 2, got.simple.size
     assert_equal 1, got.simple[0].a
