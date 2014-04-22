9c14812037603a741997170aa6f5cae04cc1cb82
Bryce Kerley <bkerley@brycekerley.net>
2014-04-22 14:18:41 -0400

    two passing benchmark tests

---
+++ test/benchmark_test.rb
@@ -0,0 +1,42 @@
+require 'minitest/autorun'
+require 'minitest/benchmark'
+require 'beefcake'
+
+class Inner
+  include Beefcake::Message
+
+  required :content, :string, 1
+end
+
+class Outer
+  include Beefcake::Message
+
+  repeated :inner, Inner, 1
+end
+
+class BenchmarkTest < Minitest::Benchmark
+  def setup
+    @buf = Beefcake::Buffer.new
+  end
+
+  def bench_read_string
+    assert_performance_linear 0.99 do |n|
+      candidate = 'x' * (n * 1_000)
+      @buf.append_string candidate
+      assert_equal candidate, @buf.read_string
+    end
+  end
+
+  def bench_decode
+    assert_performance_linear 0.99 do |n|
+      inners = n.times.map{ Inner.new :content => 'x' * 100 }
+      outer = Outer.new :inner => inners
+
+      encoded = outer.encode.to_s
+
+      decoded = Outer.decode encoded
+
+      assert_equal n, decoded.inner.length
+    end
+  end
+end
